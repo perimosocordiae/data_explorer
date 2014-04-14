f542283410e2f1bc4881dcac2e48f0dcd988de19
CJ Carey <perimosocordiae@gmail.com>
2014-04-14 11:50:57 -0400

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from distutils.core import setup
 
-version = "0.1"
+version = "0.1.1"
 setup(name='data_explorer',
       version=version,
       description='Command line utilities for data exploration',
