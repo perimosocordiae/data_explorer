c04b3293afde83e86db27de5c868879db59aa19f
CJ Carey <perimosocordiae@gmail.com>
2015-02-10 14:26:26 -0500

    More practical formats

--- data_explorer/date2unixtime.py
+++ data_explorer/date2unixtime.py
@@ -4,10 +4,14 @@ import re
 from datetime import datetime as dt
 from time import mktime
 
-fmts = ["%m %d %Y","%Y %m %d","%m %d %y","%y %m %d"]
+ALL_FMTS = {
+    2: ["%m %d"],
+    3: ["%m %d %Y","%Y %m %d","%m %d %y","%y %m %d"],
+    4: ["%a %b %d %H:%M:%S %Y"],
+}
 
 
-def try_parse(date_str):
+def try_parse(date_str, fmts):
   for i,fmt in enumerate(fmts):
     try:
       stamp = dt.strptime(date_str, fmt)
@@ -26,12 +30,9 @@ def try_parse(date_str):
 def main(fh):
   for line in fh:
     parts = filter(None,re.split('\D+',line.strip()))
-    if len(parts) == 2:
-      parts.append('1970')  # XXX: or perhaps the current year?
-    elif len(parts) != 3:
+    if len(parts) not in ALL_FMTS:
       continue
-    date = ' '.join(map(str,parts))
-    stamp = try_parse(date)
+    stamp = try_parse(' '.join(parts), ALL_FMTS[len(parts)])
     print mktime(stamp.timetuple())
 
 
