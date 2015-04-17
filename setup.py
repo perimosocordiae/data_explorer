a39cef740213a6257e3713b29c3eaac2b000f8c8
CJ Carey <perimosocordiae@gmail.com>
2015-04-17 17:57:54 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-version = "0.1.9"
+version = "0.1.10"
 setup(name='data_explorer',
       version=version,
       description='Command line utilities for data exploration',
